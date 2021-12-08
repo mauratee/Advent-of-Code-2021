e35ee8477da46cdba193bb1f8d26a9fd4b8cd6b0
Maura Thompson <thomaura@gmail.com>
2021-12-08 11:04:09 -0500

    Start string parsing for Day 4, part 1

---
+++ day04/day4_solution.py
@@ -0,0 +1,19 @@
+all_nums = []
+drawn_nums_int = []
+
+with open("day4_input.txt") as f:
+    for line in f:
+        line = line.strip('\n')
+        all_nums.append(line)
+
+f.close()
+print(all_nums[0])
+drawn_string = all_nums[0]
+
+drawn_nums = drawn_string.split(",")
+for num in drawn_nums:
+    num = int(num)
+    drawn_nums_int.append(num)
+    
+print(drawn_nums)
+print(drawn_nums_int)
